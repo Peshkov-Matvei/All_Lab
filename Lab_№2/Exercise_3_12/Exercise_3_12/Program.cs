e98b0c50cf8ce300ed51c11f735a9eb9f97651f5
Пешков Матвей <peshkov.matvei@yandex.ru>
2023-09-17 12:15:18 +0300

    fix

---
+++ Lab_№2/Exercise_3_12/Exercise_3_12/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercise_3_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r;
+            int f = 0;
+            string start = "-";
+            do
+            {
+                Console.WriteLine("Введите r");
+                r = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Выберете фигуру: 1 - квадрат, 2 - круг, 3 - р.c треугольник");
+                f = Convert.ToInt32(Console.ReadLine());
+                switch (f)
+                {
+                    case 1:
+                        Console.WriteLine(r * r);
+                        break;
+                    case 2:
+                        Console.WriteLine(r * r * Math.PI);
+                        break;
+                    case 3:
+                        Console.WriteLine(0.5 * r * r * Math.Sqrt(3) / 2);
+                        break;
+                }
+                Console.WriteLine("Введите +, если хотите продолжить");
+                Console.WriteLine("Введите -, если хотите закончить");
+                start = Console.ReadLine();
+            } while (start == "+");
+        }
+    }
+}
