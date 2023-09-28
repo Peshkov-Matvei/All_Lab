b02b76d44fc4f649aafb3dcbb1a29a0b8eb328de
Пешков Матвей <peshkov.matvei@yandex.ru>
2023-09-28 21:21:14 +0300

    add

---
+++ Lab_№3/Exercise_1_6/Exercise_1_6/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise_1_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int summa = 0;
+            int[] a = new int[5];
+            Console.WriteLine("Введите значения вектора через пробел");
+            string s = Console.ReadLine();
+            string[] c = s.Split(' ');
+            for (int i = 0; i < 5; i++)
+            {
+                a[i] = int.Parse(c[i]);
+            }
+            for (int j = 0; j < 5; j++)
+            {
+                summa += a[j];
+            }
+            Console.WriteLine(Math.Sqrt(summa));
+        }
+    }
+}
