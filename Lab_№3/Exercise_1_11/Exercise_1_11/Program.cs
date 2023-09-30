bf0186ff9b76dd1adcdc61bec7cf6cad1f903252
Пешков Матвей <peshkov.matvei@yandex.ru>
2023-09-30 20:58:46 +0300

    add

---
+++ Lab_№3/Exercise_1_11/Exercise_1_11/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise_1_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] c = new int[10];
+            Console.WriteLine("Введите 10 объектов массива через пробел");
+            string[] s = (Console.ReadLine()).Split();
+            for (int i = 0; i < 10; i++)
+            {
+                c[i] = Math.Abs(int.Parse(s[i]));
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("{0} ", c[i]);
+            }
+        }
+    }
+}
