cc103906b65a028248b8f97f6c6cce71c8da051e
Пешков Матвей <peshkov.matvei@yandex.ru>
2023-10-15 18:02:39 +0300

    fix

---
+++ Lab_№3/Exercise_3_13/Exercise_3_13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+namespace Exercise_3_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива");
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            Console.WriteLine("Введите значения массива через пробел");
+            string[] c = Console.ReadLine().Split();
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(c[i]);
+            }
+            int[] f = a.Distinct().ToArray();
+            Console.WriteLine(String.Join(" ", f));
+        }
+    }
+}
