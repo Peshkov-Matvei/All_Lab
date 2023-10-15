9590cb77c109f99de8f7392513903318bf72dc80
Пешков Матвей <peshkov.matvei@yandex.ru>
2023-10-15 17:32:47 +0300

    fix

---
+++ Lab_№3/Exercise_3_12/Exercise_3_12/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercise_3_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 12, l = 0, d = 0;
+            int[] a = new int[n];
+            Console.WriteLine("Введите 12 значений массива через пробел");
+            string[] c = Console.ReadLine().Split();
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(c[i]);
+                if (a[i] < 0) l++;
+            }
+            int[] b = new int[n - l];
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] >= 0)
+                {
+                    b[d] = a[i];
+                    Console.Write("{0} ", b[d++]);
+                }
+            }
+        }
+    }
+}
