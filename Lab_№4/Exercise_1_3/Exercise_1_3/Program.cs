293363d7fc95722fefb7f633a043b9d3fb19ff49
Пешков Матвей <peshkov.matvei@yandex.ru>
2023-10-18 00:03:27 +0300

    fix

---
+++ Lab_№4/Exercise_1_3/Exercise_1_3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise_1_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int summa = 0;
+            int[,] a = new int[4, 4] { 
+                                    { 1, 2, 3, 4},
+                                    { 5, 6, 7, 8},
+                                    { 9, 10, 11, 12},
+                                    { 13, 14, 15, 16} 
+                                    };
+            for (int i = 0; i < 4; i++)
+            {
+                summa += a[i, i] + a[3 - i, i];
+            }
+            Console.WriteLine(summa);
+        }
+    }
+}
