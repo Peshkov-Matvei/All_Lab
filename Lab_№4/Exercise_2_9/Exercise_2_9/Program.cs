6aaa93b54151c7730a8aa54f7deb554380fcee04
Пешков Матвей <peshkov.matvei@yandex.ru>
2023-11-04 19:11:01 +0300

    add

---
+++ Lab_№4/Exercise_2_9/Exercise_2_9/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercise_2_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] f = new int[6, 7] {
+                                    { 4, -23, 32, 55, -6, 0, 1},
+                                    { 10, 0, 34, 7, -14, 304, 2},
+                                    { 44, 45 ,3, -23, 14, 41, 3},
+                                    { 10, 5, 4, 333, 14, 4, 90},
+                                    { 52, 65 , 99, 44, -9, 0, 34},
+                                    { -100, 56, 36, 97, 44, 3, 77},
+                                     };
+            int c;
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    c = f[i, j];
+                    f[i, j] = f[i, 7 - j - 1];
+                    f[i, 7 - j - 1] = c;
+                }
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++) Console.Write(f[i, j] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
