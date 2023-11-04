e97e1b18f3937cc5a43e896f4c7f5c7be3f914a2
Пешков Матвей <peshkov.matvei@yandex.ru>
2023-11-04 21:04:56 +0300

    fix

--- Lab_№4/Exercise_3_1/Exercise_3_1/Program.cs
+++ Lab_№4/Exercise_3_1/Exercise_3_1/Program.cs
@@ -42,7 +42,7 @@
             } while (k > 0);
             for (int i = 0; i < 7; i++)
             {
-                for (int j = 0; j < 6; j++) Console.Write(f[i, j] + " ");
+                for (int j = 0; j < 5; j++) Console.Write(f[i, j] + " ");
                 Console.WriteLine();
             }
         }
