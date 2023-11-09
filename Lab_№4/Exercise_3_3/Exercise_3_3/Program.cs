376338cb46a6b4e82e2a966ec3940142185bfda3
Пешков Матвей <peshkov.matvei@yandex.ru>
2023-11-09 08:52:21 +0300

    fix

---
+++ Lab_№4/Exercise_3_3/Exercise_3_3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercise_3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] f = new int[5, 5] {
+                                    { 4, -23, 32, 55, -6},
+                                    { -10, -6, 91, 71, 49},
+                                    { 10, 0, 34, 7, -14},
+                                    { 10, 5, 4, 333, 14},
+                                    { 52, 65 , 99, 44, -9},
+                                     };
+            int[] d = new int[5 * 2 - 1];
+            int sum;
+            for (int t = 0; t < 9; t++)
+            {
+                sum = 0;
+                for (int i = 4; i >= 0; i--)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+
+                    }
+                }
+                d[t] = sum;
+            }
+        }
+    }
+}
---
+++ Lab_№4/Exercise_3_8/Exercise_3_8/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Exercise_3_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] f = new int[7, 5] {
+                                    { 4, -23, 32, 55, -6},
+                                    { -10, -6, 91, 71, 49},
+                                    { 10, 0, 34, 7, -14},
+                                    { 44, 45 ,3, -23, 14},
+                                    { 10, 5, 4, 333, 14},
+                                    { 52, 65 , 99, 44, -9},
+                                    { -100, -56, -36, -97, 44},
+                                     };
+            int k, d = 0;
+            int[,] r = new int[7, 6];
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    r[i, j] = f[i, j];
+                }
+                r[i, 5] = 0;
+            }
+            for (int i = 0; i < 7; i++)
+            {
+                k = 0;
+                for (int j = 0; j < 5; j++)
+                {
+                    if (r[i, j] >= 0) k++;
+                }
+                r[i, 5] = k;
+            }
+            do 
+            { 
+                k = 0;
+                for (int i = 0; i < 6; i++)
+                {
+                    if (r[i, 5] < r[i + 1, 5])
+                    {
+                        k++;
+                        for (int j = 0; j < 6; j++)
+                        {
+                            d = r[i, j];
+                            r[i, j] = r[i + 1, j];
+                            r[i + 1, j] = d;
+                        }
+                    }
+                } 
+            }while (k > 0);
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 5; j++) Console.Write(r[i, j] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
