640f5a2303bd5ce999840a8541a182c061ee6f7f
Пешков Матвей <peshkov.matvei@yandex.ru>
2023-11-09 09:08:27 +0300

    add

---
+++ Lab_№4/Exercise_3_10/Exercise_3_10/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Exercise_3_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] f = new int[5, 7] {
+                                    { 54, -23, 32, 55, -6, 0, 5 },
+                                    { 44, 45 ,3, -23, 14, 41, 6 },
+                                    { 10, 5, 4, -3, 14, 4, 6},
+                                    { 52, 65 , 99, 44, -9, 0, 9},
+                                    { -100, 56, 36, 97, 44, 3, 7},
+                                     };
+            int k, d = 0;
+            do
+            {
+                k = 0;
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j += 2)
+                    {
+                        if (f[i, j] < f[i, j + 2])
+                        {
+                            d = f[i, j];
+                            f[i, j] = f[i, j + 2];
+                            f[i, j + 2] = d;
+                            k++;
+                        }
+                    }
+                    for (int j = 1; j < 4; j += 2)
+                    {
+                        if (f[i, j] > f[i, j + 2])
+                        {
+                            d = f[i, j];
+                            f[i, j] = f[i, j + 2];
+                            f[i, j + 2] = d;
+                            k++;
+                        }
+                    }
+                }
+            } while (k > 0);
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 7; j++) Console.Write(f[i, j] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
