1510cb6db322d894d4c2d40816f8d99f90697657
Пешков Матвей <peshkov.matvei@yandex.ru>
2023-11-09 11:19:26 +0300

    add

---
+++ Lab_№4/Exercise_3_4/Exercise_3_4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercise_3_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4, t;
+            int[,] f = new int[4, 4] {
+                                    { 1, 2, 3, 4},
+                                    { 5, 6, 7, 8},
+                                    { 9, 10, 11, 12},
+                                    { 13, 14, 15, 16}
+                                    };
+            if (n % 2 != 0) t = (n + 1) / 2 - 1;
+            else t = n / 2;
+            for (int i = t; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    f[i, j] = 1;
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++) Console.Write(f[i, j] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
