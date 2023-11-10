4c86ab0c64468aed534840b4dd6339852acfbf7a
Пешков Матвей <peshkov.matvei@yandex.ru>
2023-11-10 11:56:37 +0300

    add

--- Lab_№4/Exercise_3_3/Exercise_3_3/Program.cs
+++ Lab_№4/Exercise_3_3/Exercise_3_3/Program.cs
@@ -12,19 +12,22 @@
                                     { 52, 65 , 99, 44, -9},
                                      };
             int[] d = new int[5 * 2 - 1];
-            int sum;
+            int sum, z = 0, p = 0;
             for (int t = 0; t < 9; t++)
             {
                 sum = 0;
-                for (int i = 4; i >= 0; i--)
+                for (int i = z; i < 5; i++)
                 {
-                    for (int j = 0; j < 5; j++)
+                    for (int j = p; j < 5; j++)
                     {
-
+                        sum += f[i, j];
                     }
                 }
                 d[t] = sum;
+                z += 1;
+                p += 1;
             }
+            for (int i = 0; i < 5 * 2 - 1; i++) Console.Write("{0} ", d[i]);
         }
     }
 }
