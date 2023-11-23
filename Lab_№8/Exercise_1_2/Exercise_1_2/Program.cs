e26d40726d8e69822d7351f352edcd869d3ac319
Пешков Матвей <peshkov.matvei@yandex.ru>
2023-11-23 16:59:56 +0300

    add

---
+++ Lab_№8/Exercise_1_2/Exercise_1_2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercise_1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string c, text = "Бобры живут на реке. Они отличные строители. У бобров зубы острые, как бритва. На реке бобры делают плотины из стволов осины.";
+            string[] mas = text.Split(' ');
+            for (int i = 0; i < mas.Length; i++)
+            {
+                char[] t = mas[i].ToArray();
+                Array.Reverse(t);
+                c = new string(t);
+                Console.Write(c + " ");
+                mas[i] = c;
+            }
+            Console.WriteLine();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                char[] t = mas[i].ToArray();
+                Array.Reverse(t);
+                c = new string(t);
+                Console.Write(c + " ");
+            }
+        }
+    }
+}
