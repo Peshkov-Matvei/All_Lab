133332eef46709328dce62db9c65c283c1438184
Пешков Матвей <peshkov.matvei@yandex.ru>
2023-11-30 17:19:22 +0300

    fix

--- Lab_№9/Exercise_1_1/Exercise_1_1/Program.cs
+++ Lab_№9/Exercise_1_1/Exercise_1_1/Program.cs
@@ -2,39 +2,51 @@ using System;
 using System.Text;
 using System.IO;
 
-namespace Exercise_3_4
+namespace Exercise_3_1
 {
     internal class Program
     {
-        struct Sport
+        struct Student
         {
-            public string fam;
-            public int points;
-            public Sport(string fam, int points)
+            public string familia;
+            public int rez1, rez2, rez3, rez4, rez5;
+            public double srz;
+            public Student(string familia1, int rez11, int rez21, int rez31, int rez41, int rez51)
             {
-                this.fam = fam;
-                this.points = points;
+                familia = familia1;
+                rez1 = rez11;
+                rez2 = rez21;
+                rez3 = rez31;
+                rez4 = rez41;
+                rez5 = rez51;
+                srz = (rez11 + rez21 + rez31 + rez41 + rez51) / 5.0;
+            }
+        }
+        struct Group
+        {
+            public int number;
+            public int count;
+            public double srz, sum;
+            public Group(int number1, int count1, double sum1)
+            {
+                number = number1;
+                count = count1;
+                sum = sum1;
+                srz = sum / count;
             }
         }
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             double sum1 = 0, sum2 = 0, sum3 = 0;
             Student[] s1 = new Student[3];
             Student[] s2 = new Student[2];
             Student[] s3 = new Student[2];
-=======
-            Sport[] g1 = new Sport[3];
-            Sport[] g2 = new Sport[3];
-            double amax;
->>>>>>> fe479e333228edd520398fc7c28afca17889661f
             string line;
             string path = @"text.txt";
             int w = 0;
             StreamReader sr = new StreamReader(path);
             while (w != 3)
             {
-<<<<<<< HEAD
                 line = sr.ReadLine();
                 string[] st = line.Split(' ');
                 s1[w++] = new Student(st[0], int.Parse(st[1]), int.Parse(st[2]), int.Parse(st[3]), int.Parse(st[4]), int.Parse(st[5]));
@@ -51,92 +63,107 @@ namespace Exercise_3_4
             {
                 string[] st = line.Split(' ');
                 s3[w++] = new Student(st[0], int.Parse(st[1]), int.Parse(st[2]), int.Parse(st[3]), int.Parse(st[4]), int.Parse(st[5]));
-=======
-                string[] sport = line.Split(' ');
-                g1[w].fam = sport[0];
-                g1[w].points = int.Parse(sport[1]);
-                w++;
             }
-            while ((line = sr.ReadLine()) != null)
+            for (int i = 0; i < s1.Length; i++)
             {
-                string[] sport = line.Split(' ');
-                g2[w].fam = sport[0];
-                g2[w].points = int.Parse(sport[1]);
-                w++;
->>>>>>> fe479e333228edd520398fc7c28afca17889661f
+                sum1 += s1[i].srz;
             }
-            for (int i = 0; i < g1.Length; i++)
+            for (int i = 0; i < s2.Length; i++)
             {
-                amax = g1[i].points;
-                for (int j = i + 1; j < g1.Length; j++)
+                sum2 += s2[i].srz;
+            }
+            for (int i = 0; i < s3.Length; i++)
+            {
+                sum3 += s3[i].srz;
+            }
+            Group[] g = new Group[3];
+            g[0] = new Group(1, s1.Length, sum1);
+            g[1] = new Group(2, s2.Length, sum2);
+            g[2] = new Group(3, s3.Length, sum3);
+            int k;
+            double amax;
+            for (int i = 0; i < s1.Length; i++)
+            {
+                amax = s1[i].srz;
+                for (int j = i + 1; j < s1.Length; j++)
                 {
-                    if (amax < g1[j].points)
+                    if (amax < s1[j].srz)
                     {
-                        amax = g1[j].points;
-                        Sport temp;
-                        temp = g1[j];
-                        g1[j] = g1[i];
-                        g1[i] = temp;
+                        amax = s1[j].srz;
+                        Student temp;
+                        temp = s1[j];
+                        s1[j] = s1[i];
+                        s1[i] = temp;
                     }
                 }
             }
             Console.WriteLine("Группа 1:");
-            for (int i = 0; i < g1.Length; i++)
+            for (int i = 0; i < s1.Length; i++)
             {
-                Console.WriteLine("{0} - {1}", g1[i].fam, g1[i].points);
+                if (s1[i].srz >= 4.0) Console.WriteLine("{0} - {1}", s1[i].familia, s1[i].srz);
             }
-            for (int i = 0; i < g2.Length; i++)
+            Console.WriteLine();
+            for (int i = 0; i < s2.Length; i++)
             {
-                amax = g2[i].points;
-                for (int j = i + 1; j < g2.Length; j++)
+                amax = s2[i].srz;
+                for (int j = i + 1; j < s2.Length; j++)
                 {
-                    if (amax < g2[j].points)
+                    if (amax < s2[j].srz)
                     {
-                        amax = g2[j].points;
-                        Sport temp;
-                        temp = g2[j];
-                        g2[j] = g2[i];
-                        g2[i] = temp;
+                        amax = s2[j].srz;
+                        Student temp;
+                        temp = s2[j];
+                        s2[j] = s2[i];
+                        s2[i] = temp;
                     }
                 }
             }
-            Console.WriteLine();
             Console.WriteLine("Группа 2:");
-            for (int i = 0; i < g2.Length; i++)
+            for (int i = 0; i < s2.Length; i++)
             {
-                Console.WriteLine("{0} - {1}", g2[i].fam, g2[i].points);
-            }
-            Sport[] ag = new Sport[g1.Length + g2.Length];
-            int t = 0;
-            for (; t < g1.Length; t++)
-            {
-                ag[t] = new Sport(g1[t].fam, g1[t].points);
-            }
-            for (; t < g1.Length + g2.Length; t++)
-            {
-                ag[t] = new Sport(g2[t - g1.Length].fam, g2[t - g1.Length].points);
+                if (s2[i].srz >= 4.0) Console.WriteLine("{0} - {1}", s2[i].familia, s2[i].srz);
             }
             Console.WriteLine();
-            for (int i = 0; i < ag.Length; i++)
+            for (int i = 0; i < s3.Length; i++)
             {
-                amax = ag[i].points;
-                for (int j = i + 1; j < ag.Length; j++)
+                amax = s3[i].srz;
+                for (int j = i + 1; j < s3.Length; j++)
                 {
-                    if (amax < ag[j].points)
+                    if (amax < s3[j].srz)
                     {
-                        amax = ag[j].points;
-                        Sport temp;
-                        temp = ag[j];
-                        ag[j] = ag[i];
-                        ag[i] = temp;
+                        amax = s3[j].srz;
+                        Student temp;
+                        temp = s3[j];
+                        s3[j] = s3[i];
+                        s3[i] = temp;
                     }
                 }
             }
-            Console.WriteLine();
-            Console.WriteLine("Список участников соревнования:");
-            for (int i = 0; i < ag.Length; i++)
+            Console.WriteLine("Группа 3:");
+            for (int i = 0; i < s3.Length; i++)
             {
-                Console.WriteLine("{0} - {1}", ag[i].fam, ag[i].points);
+                if (s3[i].srz >= 4.0) Console.WriteLine("{0} - {1}", s3[i].familia, s3[i].srz);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < g.Length; i++)
+            {
+                amax = g[i].srz;
+                for (int j = i + 1; j < g.Length; j++)
+                {
+                    if (amax < g[j].srz)
+                    {
+                        amax = g[j].srz;
+                        Group temp;
+                        temp = g[j];
+                        g[j] = g[i];
+                        g[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Список групп:");
+            for (int i = 0; i < g.Length; i++)
+            {
+                Console.WriteLine("Группа под номером - {0} имеет средний балл - {1:f2}", g[i].number, g[i].srz);
             }
         }
     }
