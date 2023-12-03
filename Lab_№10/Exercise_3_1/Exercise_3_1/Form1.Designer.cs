9687e4ab6e30c84fec48029cc6465185148297b0
Пешков Матвей <peshkov.matvei@yandex.ru>
2023-12-03 17:18:15 +0300

    finish

---
+++ Lab_№10/Exercise_3_1/Exercise_3_1/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Exercise_3_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(464, 367);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сделать ход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(78, 261);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Первый игрок";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(287, 261);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "Второй игрок";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(464, 261);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.Text = "Третий игрок";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(39, 45);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(285, 180);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(385, 45);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(303, 180);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = resources.GetString("richTextBox2.Text");
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(879, 310);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Десятый игрок";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(464, 310);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "Восьмой игрок";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(287, 310);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.Text = "Седьмой игрок";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(674, 310);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 10;
+            this.textBox7.Text = "Девятый игрок";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(674, 261);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 11;
+            this.textBox8.Text = "Четвертый игрок";
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(78, 310);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 12;
+            this.textBox9.Text = "Шестой игрок";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(879, 261);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 13;
+            this.textBox10.Text = "Пятый игрок";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(763, 45);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(248, 180);
+            this.richTextBox3.TabIndex = 14;
+            this.richTextBox3.Text = resources.GetString("richTextBox3.Text");
+            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 450);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+    }
+}
+
---
+++ Lab_№10/Exercise_3_1/Exercise_3_1/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Exercise_3_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Red;
+            Random rn = new Random();
+            string[] text = richTextBox2.Text.Split(' ', '\n'), text1 = richTextBox3.Text.Split(' ', '\n');
+            int n = int.Parse(text[2]), n1 = int.Parse(text[8]), n2 = int.Parse(text[15]), n3 = int.Parse(text[22]), n4 = int.Parse(text[28]), n5 = int.Parse(text[34]),
+                n6 = int.Parse(text[40]), n7 = int.Parse(text[46]), n8 = int.Parse(text[52]), n9 = int.Parse(text[58]), n10 = int.Parse(text[64]);
+            int r = int.Parse(text1[2]), n11 = int.Parse(text1[8]), n22 = int.Parse(text1[15]), n33 = int.Parse(text1[22]), n44 = int.Parse(text1[28]), n55 = int.Parse(text1[34]),
+                n66 = int.Parse(text1[40]), n77 = int.Parse(text1[46]), n88 = int.Parse(text1[52]), n99 = int.Parse(text1[58]), n100 = int.Parse(text1[64]);
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+            richTextBox3.Clear();
+            if (n == 5)
+            {
+                n = 0; r++;
+                int[] s1 = { n1, n2, n3, n4, n5, n6, n7, n8, n9, n10 };
+                for (int i = 0; i < s1.Length; i++)
+                {
+                    if (s1.Max() == s1[i])
+                    {
+                        if (i == 0) n11 += 5;
+                        if (i == 1) n22 += 5;
+                        if (i == 2) n33 += 5;
+                        if (i == 3) n44 += 5;
+                        if (i == 4) n55 += 5;
+                        if (i == 5) n66 += 5;
+                        if (i == 6) n77 += 5;
+                        if (i == 7) n88 += 5;
+                        if (i == 8) n99 += 5;
+                        if (i == 9) n100 += 5;
+                    }
+                }
+                n1 = 0; n2 = 0; n3 = 0; n4 = 0; n5 = 0;
+                n6 = 0; n7 = 0; n8 = 0; n9 = 0; n10 = 0;
+            }
+            else 
+            { 
+                n++; n1 += rn.Next(1, 6); n2 += rn.Next(1, 6); n3 += rn.Next(1, 6); n4 += rn.Next(1, 6); n10 += rn.Next(1, 6);
+                n5 += rn.Next(1, 6); n6 += rn.Next(1, 6); n7 += rn.Next(1, 6); n8 += rn.Next(1, 6); n9 += rn.Next(1, 6);
+
+            }
+            richTextBox1.AppendText("Первый игрок - " + textBox2.Text + "\n \n");
+            richTextBox1.AppendText("Второй игрок - " + textBox3.Text + "\n \n");
+            richTextBox1.AppendText("Третий игрок - " + textBox4.Text + "\n \n");
+            richTextBox1.AppendText("Четвертый игрок - " + textBox8.Text + "\n \n");
+            richTextBox1.AppendText("Пятый игрок - " + textBox10.Text + "\n \n");
+            richTextBox1.AppendText("Шестой игрок - " + textBox9.Text + "\n \n");
+            richTextBox1.AppendText("Седьмой игрок - " + textBox6.Text + "\n \n");
+            richTextBox1.AppendText("Восьмой игрок - " + textBox5.Text + "\n \n");
+            richTextBox1.AppendText("Девятый игрок - " + textBox7.Text + "\n \n");
+            richTextBox1.AppendText("Десятый игрок - " + textBox1.Text);
+            richTextBox2.AppendText("Ход - " + n + "\n \n");
+            richTextBox2.AppendText("Первый игрок имеет " + n1 + " очков" + "\n \n");
+            richTextBox2.AppendText("Второй игрок имеет " + n2 + " очков" + "\n \n");
+            richTextBox2.AppendText("Третий игрок имеет " + n3 + " очков" + "\n \n");
+            richTextBox2.AppendText("Четвертый имеет " + n4 + " очков" + "\n \n");
+            richTextBox2.AppendText("Пятый имеет " + n5 + " очков" + "\n \n");
+            richTextBox2.AppendText("Шестой имеет " + n6 + " очков" + "\n \n");
+            richTextBox2.AppendText("Седьмой имеет " + n7 + " очков" + "\n \n");
+            richTextBox2.AppendText("Восьмой имеет " + n8 + " очков" + "\n \n");
+            richTextBox2.AppendText("Девятый имеет " + n9 + " очков" + "\n \n");
+            richTextBox2.AppendText("Десятый имеет " + n10 + " очков");
+            richTextBox3.AppendText("Раунд - " + r + "\n \n");
+            richTextBox3.AppendText("Первый игрок имеет " + n11 + " баллов" + "\n \n");
+            richTextBox3.AppendText("Второй игрок имеет " + n22 + " баллов" + "\n \n");
+            richTextBox3.AppendText("Третий игрок имеет " + n33 + " баллов" + "\n \n");
+            richTextBox3.AppendText("Четвертый имеет " + n44 + " баллов" + "\n \n");
+            richTextBox3.AppendText("Пятый имеет " + n55 + " баллов" + "\n \n");
+            richTextBox3.AppendText("Шестой имеет " + n66 + " баллов" + "\n \n");
+            richTextBox3.AppendText("Седьмой имеет " + n77 + " баллов" + "\n \n");
+            richTextBox3.AppendText("Восьмой имеет " + n88 + " баллов" + "\n \n");
+            richTextBox3.AppendText("Девятый имеет " + n99 + " баллов" + "\n \n");
+            richTextBox3.AppendText("Десятый имеет " + n100 + " баллов");
+            if (r == 5)
+            {
+                int k = 0;
+                int[] s2 = { n11, n22, n33, n44, n55, n66, n77, n88, n99, n100};
+                for (int i = 0; i < s2.Length; i++)
+                {
+                    if (s2.Max() == s2[i])
+                    {
+                        if (k == 0)
+                        {
+                            MessageBox.Show("Игрок под номером " + Convert.ToString(i + 1) + " выйграл!!!!");
+                            k++;
+                        }
+                        else MessageBox.Show("Игрок под номером " + Convert.ToString(i + 1) + " тоже выйграл!!!!!");
+                    }
+                }
+                richTextBox2.Clear();
+                richTextBox3.Clear();
+                richTextBox2.AppendText("Ход - " + 0 + "\n \n");
+                richTextBox2.AppendText("Первый игрок имеет " + 0 + " очков" + "\n \n");
+                richTextBox2.AppendText("Второй игрок имеет " + 0 + " очков" + "\n \n");
+                richTextBox2.AppendText("Третий игрок имеет " + 0 + " очков" + "\n \n");
+                richTextBox2.AppendText("Четвертый имеет " + 0 + " очков" + "\n \n");
+                richTextBox2.AppendText("Пятый имеет " + 0 + " очков" + "\n \n");
+                richTextBox2.AppendText("Шестой имеет " + 0 + " очков" + "\n \n");
+                richTextBox2.AppendText("Седьмой имеет " + 0 + " очков" + "\n \n");
+                richTextBox2.AppendText("Восьмой имеет " + 0 + " очков" + "\n \n");
+                richTextBox2.AppendText("Девятый имеет " + 0 + " очков" + "\n \n");
+                richTextBox2.AppendText("Десятый имеет " + 0 + " очков");
+                richTextBox3.AppendText("Раунд - " + 0 + "\n \n");
+                richTextBox3.AppendText("Первый игрок имеет " + 0 + " баллов" + "\n \n");
+                richTextBox3.AppendText("Второй игрок имеет " + 0 + " баллов" + "\n \n");
+                richTextBox3.AppendText("Третий игрок имеет " + 0 + " баллов" + "\n \n");
+                richTextBox3.AppendText("Четвертый имеет " + 0 + " баллов" + "\n \n");
+                richTextBox3.AppendText("Пятый имеет " + 0 + " баллов" + "\n \n");
+                richTextBox3.AppendText("Шестой имеет " + 0 + " баллов" + "\n \n");
+                richTextBox3.AppendText("Седьмой имеет " + 0 + " баллов" + "\n \n");
+                richTextBox3.AppendText("Восьмой имеет " + 0 + " баллов" + "\n \n");
+                richTextBox3.AppendText("Девятый имеет " + 0 + " баллов" + "\n \n");
+                richTextBox3.AppendText("Десятый имеет " + 0 + " баллов");
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
